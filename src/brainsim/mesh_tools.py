54bdbfb0d3f67e0f5d49c30c5e00d32e1ac5b707
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2021-04-15 12:37:45 +0200

    Fixed so CRAS is computed from nifti-image

--- src/brainsim/mesh_tools.py
+++ src/brainsim/mesh_tools.py
@@ -4,35 +4,44 @@ import numpy as np
 import SVMTK as svmtk
 
 
-def transform_coords(coords, transform):
+def transform_coords(coords, transform, inverse=False):
+    if inverse:
+        transform = np.linalg.inv(transform)
     coords = np.asarray(coords)
     if coords.ndim == 2:
         coords = coords.T
-    coords = transform[:3, :3]@coords + transform[:3, 3, np.newaxis]
-    if coords.ndim == 2:
+        coords = transform[:3, :3]@coords + transform[:3, 3, np.newaxis]
         coords = coords.T
+    else:
+        coords = transform[:3, :3] @ coords + transform[:3, 3]
+
     return coords
 
 
-def get_surface_ras_to_image_coordinates_transform(surface_metadata, image_nii):
+def get_cras(image_nii):
+    """Get the center of the RAS coordinate system.
+
+    Based on: https://neurostars.org/t/freesurfer-cras-offset/5587/2
+    Which links to: https://github.com/nipy/nibabel/blob/d1518aa71a8a80f5e7049a3509dfb49cf6b78005/nibabel/freesurfer/mghformat.py#L630-L643
+    """
+    shape = np.array(image_nii.shape)
+    center = shape / 2
+    center_homogeneous = np.hstack((center, [1]))
+    transform = image_nii.affine
+    return (transform @ center_homogeneous)[:3]
+
+
+def get_surface_ras_to_image_coordinates_transform(image_nii):
     """Convert from freesurfer surface coordinates to scanner coordinates.
 
     Freesurfer uses (at least) three different coordinate systems, the RAS system,
-    the surface RAS system (which is a shifted version of the RAS system) and the
-    image coordinates. This function creates a transformation matrix that transforms
-    the surface RAS system into image coordinates. To accomplish this, it uses the
-    cras (center of RAS) property from the surface metadata to translate the surface
-    RAS into the correct RAS coordinates. Then it uses the inverse of the image
-    coordinate to RAS transformation to transform the RAS coordinates into image
-    coordinates.
+    the surface RAS system (which is the RAS system shifted so the center is in the origin)
+    and the image coordinates. This function creates a transformation matrix that transforms
+    the surface RAS system into image coordinates. To accomplish this, it first computes the
+    c_ras (center of RAS system) to translate the surface RAS into the correct RAS coordinates.
+    Then it uses the inverse of the image coordinate to RAS transformation to transform the 
+    RAS coordinates into image coordinates.
 
-    The surface metadata is obtained from using the ``nibabel.freesurfer.read_geometry``
-    function like this:
-
-    .. code::
-
-        points, faces, metadata = nib.freesurfer.read_geometry(path, read_metadata=True)
-    
     The image coordinate transform is the affine transform in the nifti file whose image
     coordinates we want to transform into.
 
@@ -40,12 +49,11 @@ def get_surface_ras_to_image_coordinates_transform(surface_metadata, image_nii):
     -------
 
     >>> import nibabel as nib
-    ... points, faces, metadata = nib.freesurfer.read_geometry("lh.pial", read_metadata=True)
     ... img = nib.load("T2W.nii")
-    ... translation_matrix = get_surface_ras_to_image_coordinates_transform(metadata, img)
+    ... translation_matrix = get_surface_ras_to_image_coordinates_transform(img)
     """
     translation_matrix = np.eye(4)
-    translation_matrix[:3, -1] = surface_metadata['cras']
+    translation_matrix[:3, -1] = get_cras(image_nii)
 
     return np.linalg.inv(image_nii.affine)@translation_matrix
 
@@ -55,7 +63,6 @@ def process_surface(input_file, output_file):
     surface = svmtk.Surface(str(input_file))
     yield surface
     surface.save(str(output_file))
-
 
 
 def create_volume_mesh(input, output_file, resolution=16):
