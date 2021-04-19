ea2d2413f9464437555a8cdb1070848e2d8eefca
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2021-04-19 19:01:27 +0200

    Added option to specify mesh metadata in generating surface RAS to voxel transformation

--- src/brainsim/mesh_tools.py
+++ src/brainsim/mesh_tools.py
@@ -18,7 +18,7 @@ def transform_coords(coords, transform, inverse=False):
     return coords
 
 
-def get_cras(image_nii):
+def get_cras_from_nifti(image_nii):
     """Get the center of the RAS coordinate system.
 
     Based on: https://neurostars.org/t/freesurfer-cras-offset/5587/2
@@ -31,29 +31,44 @@ def get_cras(image_nii):
     return (transform @ center_homogeneous)[:3]
 
 
-def get_surface_ras_to_image_coordinates_transform(image_nii):
+def get_surface_ras_to_image_coordinates_transform(image_nii, surface_metadata=None):
     """Convert from freesurfer surface coordinates to scanner coordinates.
 
     Freesurfer uses (at least) three different coordinate systems, the RAS system,
-    the surface RAS system (which is the RAS system shifted so the center is in the origin)
-    and the image coordinates. This function creates a transformation matrix that transforms
-    the surface RAS system into image coordinates. To accomplish this, it first computes the
-    c_ras (center of RAS system) to translate the surface RAS into the correct RAS coordinates.
-    Then it uses the inverse of the image coordinate to RAS transformation to transform the 
-    RAS coordinates into image coordinates.
+    the surface RAS system (which is a shifted version of the RAS system) and the
+    image coordinates. This function creates a transformation matrix that transforms
+    the surface RAS system into image coordinates. To accomplish this, it uses the
+    cras (center of RAS) property from the surface metadata to translate the surface
+    RAS into the correct RAS coordinates. Then it uses the inverse of the image
+    coordinate to RAS transformation to transform the RAS coordinates into image
+    coordinates.
 
+    The surface metadata is obtained from using the ``nibabel.freesurfer.read_geometry``
+    function like this:
+
+        points, faces, metadata = nib.freesurfer.read_geometry(path, read_metadata=True)
+    
     The image coordinate transform is the affine transform in the nifti file whose image
     coordinates we want to transform into.
+
+    If the metadata is not supplied, then it is assumed to be created from an image with
+    the same coordinate systems as the given nifti file.
 
     Example
     -------
 
+
     >>> import nibabel as nib
+    ... points, faces, metadata = nib.freesurfer.read_geometry("lh.pial", read_metadata=True)
     ... img = nib.load("T2W.nii")
+    ... translation_matrix = get_surface_ras_to_image_coordinates_transform(img, metadata)
     ... translation_matrix = get_surface_ras_to_image_coordinates_transform(img)
     """
     translation_matrix = np.eye(4)
-    translation_matrix[:3, -1] = get_cras(image_nii)
+    if surface_metadata is not None:
+        translation_matrix[:3, -1] = surface_metadata['cras']
+    else:
+        translation_matrix[:3, -1] = get_cras_from_nifti(image_nii)
 
     return np.linalg.inv(image_nii.affine)@translation_matrix
 
