1df8d58034df690d7b9934979858b1a85d619085
Yngve Mardal Moe <yngve.m.moe@gmail.com>
2021-04-19 19:01:47 +0200

    Changed default origin of image mesh

--- src/brainsim/vtk_utils.py
+++ src/brainsim/vtk_utils.py
@@ -12,7 +12,7 @@ def get_transform_matrix(transform):
 def to_pyvista_grid(image_stack, name="Scalars_", spacing=(1, 1, 1)):
     image = pv.UniformGrid()
     image.dimensions = np.array(image_stack.shape)
-    image.origin = np.array(image_stack.shape)/2
+    image.origin = np.zeros_like(image_stack.shape)
     image.spacing = spacing
     image.point_arrays[name] = image_stack.ravel('F')
     return image
